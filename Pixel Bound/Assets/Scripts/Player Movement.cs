af473f6c70513b5e9db00b563139e109cac499cc
rohithnafeel <devarc16@gmail.com>
2026-07-16 00:04:26 +0530

    Update Player Movement.cs

--- Pixel Bound/Assets/Scripts/Player Movement.cs
+++ Pixel Bound/Assets/Scripts/Player Movement.cs
@@ -6,13 +6,11 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 8f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
